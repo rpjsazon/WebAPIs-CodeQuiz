d7f1295177e4a3a4dbb5bb7573629e3ab0b62d95
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-01-13 15:23:41 +0000

    questions are added to questions.js

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,6 +1,48 @@
-var quiz = {
-  question: "",
-  answers: [
-    
-  ]
-}
+// Question added with boolean to check if the answers are correct or incorrect
+var quiz = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<src>", correct: false },
+      { text: "<script?", correct: true }
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<src>", correct: false },
+      { text: "<script?", correct: true }
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<src>", correct: false },
+      { text: "<script?", correct: true }
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<src>", correct: false },
+      { text: "<script?", correct: true }
+    ]
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<src>", correct: false },
+      { text: "<script?", correct: true }
+    ]
+  },
+];
