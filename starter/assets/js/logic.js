a758f5aba2dd76b6e65bf0c6c94994ae0cc20235
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-01-14 15:00:07 +0000

    timer, question, endquiz function is now working

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,6 +1,8 @@
 var currentQuestion = 0;
 var score = 0;
 var timeRemaining = 60;
+// Variable for time interbal 1000ms that will be set later.
+var timeInterval;
 
 
 // Initialized index.html Line 25 <button id="start">Start Quiz</button>
@@ -9,28 +11,34 @@ startListen.addEventListener("click", startQuiz);
 
 // When the "Start Quiz" is pressed, hide the display and display the questions.
 function startQuiz() {
-  displayQuestion();
   startTimer();
+  displayQuestion();
+  timeInterval = setInterval(startTimer, 1000);
 }
-
 
 // Initialized the timer once the "Start Quiz" is pressed.
 // <div class="timer">Time: <span id="time">0</span></div> 
 function startTimer() {
-  setInterval(function(){ 
-    timeRemaining--;
-    // Display the timer to index.html
-    document.querySelector("#time").textContent = timeRemaining;
-    // I set the time remaining to 60 seconds and if hit 0 then call function endGame or highscore.html
-    if (timeRemaining <= 0) {
-      endQuiz();
-    }
-  }, 1000);
+  timeRemaining--;
+  document.querySelector("#time").textContent = timeRemaining;
+  if (timeRemaining <= 0 || currentQuestion === quiz.length) {
+    clearInterval(timeInterval);
+    endQuiz();
+  }
+
+  // This timer does not stop.
+  // setInterval(function(){ 
+  //   timeRemaining--;
+  //   // Display the timer to index.html
+  //   document.querySelector("#time").textContent = timeRemaining;
+  //   // I set the time remaining to 60 seconds and if hit 0 then call function endGame or highscore.html
+  //   if (timeRemaining === 0) {
+  //     endQuiz();
+  //   }
+  // }, 1000);
 }
 
-// How can I display the questions to the HTML, I was thinking to change the P tags instead of
-// the div question.
-// Create div? 
+// Display questions array from the question.js 
 function displayQuestion(){
   // Calling objects in question.js
   var currentQ = quiz[currentQuestion];
@@ -46,6 +54,7 @@ function displayQuestion(){
   // Re-watched Zoom Meeting with Dan about JSON stringify and how to use it to convert object to string.
   questionElement.textContent = JSON.stringify(currentQ.question);
 
+  // Manipulate div "choices" and add a button to html.
   var answerElement = document.querySelector("#choices");
   answerElement.style.display = "block";
   answerElement.innerHTML="";
@@ -58,17 +67,21 @@ function displayQuestion(){
     // Need to figure out how to display the submit button 2:02AM Friday
     // Need to start here first thing in the morning.
     button.addEventListener("click", function(){
+      // if answers is equal to correctAnser in the array of question then return with score.
       if (answer === currentQ.correctAnswer) {
-        score += 25;
+        score += 20;
         document.querySelector("#scoredisplay").textContent = score;
+        
       } else {
+        // If incorrect then -10 seconds (penalize).
         timeRemaining -= 10;
       }
+      // To continue reading array, it will kept adding until it reached the max length.
       currentQuestion++;
-      if (currentQuestion === questions.length) {
-        endQuiz();
-      } else {
+      if (currentQuestion < quiz.length) {
         displayQuestion();
+      } else {
+        endQuiz();
       }
     });
     answerElement.appendChild(button);
@@ -78,7 +91,6 @@ function displayQuestion(){
 // To end the quiz. Once, the timer remaining hit 0 then show the final score.
 function endQuiz() {
   // Reference: https://developer.mozilla.org/en-US/docs/Web/API/clearInterval
-  clearInterval();
   var endScreen = document.querySelector("#end-screen");
   endScreen.style.display = "block";
   document.querySelector("#final-score").textContent = score;
