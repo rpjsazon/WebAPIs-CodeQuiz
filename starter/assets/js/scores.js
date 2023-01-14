8ae26277e23d96e67af9aadd5ca8a6fb8cbc569a
Robert Patrick Jordan Sazon <rpjsazon@gmail.com>
2023-01-14 21:05:13 +0000

    highscore list not working

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,10 +1,25 @@
-// Reference: https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
-var submitScore = document.querySelector("#submit");
-submitScore.addEventListener("click", function() {
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+// It is not working.
+function submitS() {
+  var newitem = document.createElement("ol");
+  // var newname = document.querySelector("#initials-name").value;
+
+  var newname = document.createTextNode("This is not working");
+
+  newitem.appendChild(newname);
+
+  var currentHS = document.querySelector("#highscores");
+  document.body.insertBefore(newitem, currentHS);
   
-  window.location.href = "./highscores.html";
-  var displayPlayer = document.createElement('div');
-  var initials = document.getElementById("#initials").value;
-  localStorage.setItem("initials", initials);
-  localStorage.setItem("score", score);
-});
+  // var nameList = document.getElementById("#highscores");
+  // nameList.textContent(newListItem);
+}
+
+// var submitScore = document.querySelector("#submit");
+// submitScore.addEventListener("click", function() {
+//   var initials = document.getElementById("#initials").value;
+//   var playerHighscores = document.querySelector("#highscores");
+//   var list = document.createElement("li");
+//   list.textContent = initials;
+//   playerHighscores.appendChild(list);
+// });
